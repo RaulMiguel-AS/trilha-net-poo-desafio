6e9d54e130ed58a42b73bb16f5196780ccd65f4b
RaulMiguel-AS <raulm.alvessoares@gmail.com>
2024-01-10 10:02:41 -0300

    Herança

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,6 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
+        protected string IMEI;
+        protected string Modelo; 
+        protected int Memoria; 
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
 
         public Smartphone(string numero)
